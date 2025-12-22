fe719ff609ae3f524cb7c30bf5a7a17bbb99cf68
scriptWithKishan <kishan.2785.2003@gmail.com>
2025-12-22 14:50:23 +0000

    02: Frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <Button variant="outline">Click me!</Button>
+    </div>
+  )
+}
+
+export default App
